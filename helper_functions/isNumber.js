3b2149589163f273c2dfa9d3a857353fc800b721
Okoronkwo Daniel <danielchukwuemela751@gmail.com>
2025-02-04 03:39:32 +0100

    Made some changes to the isNumber.js file and properties.js file

--- helper_functions/isNumber.js
+++ helper_functions/isNumber.js
@@ -1,3 +1,4 @@
-const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string);
+// const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string);
+const isNumeric = (string) => /^[+-]?\d+$/.test(string);
 
 module.exports = {isNumeric}
--- helper_functions/properties.js
+++ helper_functions/properties.js
@@ -1,7 +1,5 @@
 const returnProperties = (num) => {
     const properties = []
-    is_neg = false; // this is flag so that negative numbers will never have armstrong as a property
-    if(num.includes("-")) is_neg = true;
     // if the number is negative, replace the -ve sign
     num = num.replace("-", "");
 
@@ -12,7 +10,7 @@ const returnProperties = (num) => {
     // check if the sum of the cubes of the number is equal to num
     const sum_of_powers = num_array.reduce((total, item) => total += item**(num_array.length), 0); 
 
-    if(sum_of_powers === parseInt(num) && !is_neg) properties.push("armstrong");    // if the number sum_of_powers is equal to the number and the number is not negative, then the number can be an armstrong number
+    if(sum_of_powers === parseInt(num)) properties.push("armstrong");    // if the number sum_of_powers is equal to the number and the number is not negative, then the number can be an armstrong number
 
     if (num % 2 === 0) properties.push("even"); 
         else properties.push("odd");
