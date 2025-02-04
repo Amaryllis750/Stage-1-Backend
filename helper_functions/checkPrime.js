7f513fb400c24a343e90bd8a459ff806c22b7a19
Okoronkwo Daniel <danielchukwuemela751@gmail.com>
2025-02-04 05:56:38 +0100

    Corrected the isPrime function so that 6 is now seen as a prime number

--- helper_functions/checkPrime.js
+++ helper_functions/checkPrime.js
@@ -1,29 +1,30 @@
-function range(start, end, step = 1){
-    return Array.from({length: Math.ceil((end - start) / step)}, (_, i) => start + (i * step));
-}
-
 const checkPrime = (num) => {
-    num = parseInt(num); // change the number from a string to a number
-    if (num < 0) return false;  // negative numbers cannot be prime
-    num += 1;
+    // convert the number from a string to number
+    num = parseInt(num);
 
-    // using the sieve of eratosthenes
-    if(num === 2) return true
+    // the sieve is designed to check for primes before the number num, therefore num won't be included
+    // therefore, instead of the length of our array being of num length, it will be of num + 1 length
+    if (num === 2) return true
+    const array_length = num + 1;
 
-    // create an array 
-    const is_prime = new Array(num).fill(true);     // this contains an array from the number 0 to the number n
-    is_prime[0] = false;
-    is_prime[1] = false;
+    // create an array of num + 1 and then set everything to true
+    const primes = Array(num + 1).fill(true);
+    // set 0 and 1 to false, since they are not primes
+    primes[0] = false;
+    primes[1] = false;
 
-    for(let x of range(2, Math.floor(Math.sqrt(num)))){
-        if(is_prime[x]){
-            for (let y of range(x * x, num, x)){
-                is_prime[y] = false;
+    // start going through the rest of the array, start from 2 and end at the square root of the number n + 1
+    for(let i = 2; i <= Math.sqrt(array_length); i++){
+        // if the value at this index is true
+        if(primes[i]){
+            // start from the square of the index (an optimization technique)
+            for(let j = i * i; j < array_length; j += i){
+                primes[j] = false;
             }
         }
     }
 
-    return is_prime[is_prime.length - 1];
+    return primes[num];
 }
 
 module.exports = {checkPrime}
